bd02e2dea833ef5277f048af547846cabc5f38f9
Taylor McCants <taylor.mccants@uzh.ch>
2021-05-28 21:32:42 +0200

    set default configuration for apis

---
+++ ui/src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    resortServiceUrl: 'http://localhost:8082',
+    positionStackAPIKey: '4c2c6f0c599090b04e3f31232468ee5f',
+    positionStackUrl: 'http://api.positionstack.com/v1'
+};
