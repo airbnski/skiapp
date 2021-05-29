dea8cdf48116aec9f9ec4520d735ecbdae6240af
shobuxtreme <shubhankar.joshi.sj@gmail.com>
2021-05-29 23:11:31 +0200

    Docker compose added

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    resortServiceUrl: 'http://localhost:8082',
+    //resortServiceUrl: 'http://localhost:8082',
+    resortServiceUrl: 'http://resort',    
     positionStackAPIKey: '4c2c6f0c599090b04e3f31232468ee5f',
     positionStackUrl: 'http://api.positionstack.com/v1'
 };
