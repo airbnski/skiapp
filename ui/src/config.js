8a2286cb5ca76a6bd84b8660f1204385294baf2d
Shubhankar Joshi <40591587+shobuxtreme@users.noreply.github.com>
2021-05-30 04:04:52 +0200

    Update config.js

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     //resortServiceUrl: 'http://localhost:8082',
-    resortServiceUrl: 'http://resort:8082',    
+    //Production: 
+    resortServiceUrl: 'http://34.65.190.202:8082', 
     positionStackAPIKey: '4c2c6f0c599090b04e3f31232468ee5f',
     positionStackUrl: 'http://api.positionstack.com/v1'
 };
