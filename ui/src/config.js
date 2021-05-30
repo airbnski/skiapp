3aee7fe47f5e4028d029b1eccfcecaf736042365
Taylor McCants <taylor.mccants@uzh.ch>
2021-05-30 11:03:20 +0200

    update config with google maps api key

--- ui/src/config.js
+++ ui/src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     //resortServiceUrl: 'http://localhost:8082',
     resortServiceUrl: 'http://airbnski:8082',    
     positionStackAPIKey: '4c2c6f0c599090b04e3f31232468ee5f',
-    positionStackUrl: 'http://api.positionstack.com/v1'
+    positionStackUrl: 'http://api.positionstack.com/v1',
+    googleMapsApiKey: 'AIzaSyD3d6K7s9hUo-RcxyA9VLn3gag2MeNFi4A'
 };
